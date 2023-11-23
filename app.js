5fa2e0e2dba5c5fe0b206c810458cf9fe7b8302d
Álvaro Cabo <criptocrazy8@gmail.com>
2023-11-23 01:54:25 +0100

    Firebase works

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// Import the functions you need from the SDKs you need
+import {
+  initializeApp,
+  getApp,
+  getApps,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getFirestore,
+  addDoc,
+  serverTimestamp,
+  collection,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU8j7sOzwdOgXqrlQVjbgfpbbTIcuKd3k",
+  authDomain: "thalos-ada72.firebaseapp.com",
+  projectId: "thalos-ada72",
+  storageBucket: "thalos-ada72.appspot.com",
+  messagingSenderId: "334285141266",
+  appId: "1:334285141266:web:2214bab5eacee27a8c6fcb",
+  measurementId: "G-KQVEDNEJK8",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
 const hamburger = document.querySelector(
   ".header .nav-bar .nav-list .hamburger"
 );
@@ -28,51 +56,80 @@ menu_item.forEach((item) => {
   });
 });
 
-const popup = document.getElementById('popup');
-const closeBtn = document.getElementById('popup-close-btn');
+const popup = document.getElementById("popup");
+const closeBtn = document.getElementById("popup-close-btn");
 
-closeBtn.addEventListener('click', function() {
+closeBtn.addEventListener("click", function () {
   // Close the popup with a smooth transition
-  popup.classList.remove('active');
+  popup.classList.remove("active");
 });
-  const emailForm = document.getElementById('emailForm');
+const emailForm = document.getElementById("emailForm");
 
-  // Get the existing CTA button
-  const ctaButton = document.getElementById('horus-pop-up');
+// Get the existing CTA button
+const ctaButton = document.getElementById("horus-pop-up");
 
-  ctaButton.addEventListener('click', function(e) {
-    // Prevent the default link behavior
-    e.preventDefault();
-  
-    // Show the popup with a smooth transition
-    popup.classList.add('active');
-  });
+ctaButton.addEventListener("click", function (e) {
+  // Prevent the default link behavior
+  e.preventDefault();
 
-  // Attach a click event listener to the CTA button
-  ctaButton.addEventListener('click', function(e) {
-    // Prevent the default link behavior
-    e.preventDefault();
+  // Show the popup with a smooth transition
+  popup.classList.add("active");
+});
 
-    // Show the popup
-    popup.style.display = 'block';
-  });
+// Attach a click event listener to the CTA button
+ctaButton.addEventListener("click", function (e) {
+  // Prevent the default link behavior
+  e.preventDefault();
 
-  // Attach a submit event listener to the form
-  emailForm.addEventListener('submit', function(e) {
-    // Prevent the form from submitting in the traditional way
-    e.preventDefault();
+  // Show the popup
+  popup.style.display = "block";
+});
 
-    // Get the entered email value
-    const userEmail = document.getElementById('email').value;
+emailForm.addEventListener("submit", function (e) {
+  // Prevent the form from submitting in the traditional way
+  e.preventDefault();
 
-    // Simple email validation
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Get the entered email value
+  const userEmail = document.getElementById("email").value;
 
-    if (emailRegex.test(userEmail)) {
-      // If the email is valid, redirect the user to the specified link
-      window.location.href = "https://t.me/horus_vbot";
-    } else {
-      // If the email is not valid, you can display an error message or take other actions
-      alert('Please enter a valid email address.');
-    }
-  });
+  // Simple email validation
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailRegex.test(userEmail)) {
+    // If the email is valid, store it in Firebase
+    addEmailToFirestore(userEmail)
+      .then(function (success) {
+        const urlToOpen = "https://t.me/horus_vbot";
+
+        // Open the URL in a new tab
+        if (success) {window.open(urlToOpen, "_blank");
+        popup.classList.remove("active");
+      }
+        // Redirect the user to the specified link
+        // window.location.href = "https://t.me/horus_vbot";
+      })
+      .catch(function (error) {
+        console.error("Error adding email to Firebase:", error);
+      });
+  } else {
+    // If the email is not valid, you can display an error message or take other actions
+    alert("Please enter a valid email address.");
+  }
+});
+
+export async function addEmailToFirestore(email) {
+  const mailsRef = collection(db, "mails");
+  try {
+    // Add the email and timestamp to the 'userEmails' collection
+    const docRef = await addDoc(mailsRef, {
+      email,
+      timestamp: serverTimestamp(),
+    });
+
+    console.log("Email stored in Firestore with ID:", docRef.id);
+    return true;
+  } catch (error) {
+    console.error("Error adding email to Firestore:", error);
+    return false;
+  }
+}
