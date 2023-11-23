a0747c19f677b1bd98ee1b6d52fdce0ecab6ffd0
Álvaro Cabo <criptocrazy8@gmail.com>
2023-11-23 01:22:30 +0100

    TODO:Integrate firebase

--- app.js
+++ app.js
@@ -28,9 +28,51 @@ menu_item.forEach((item) => {
   });
 });
 
+const popup = document.getElementById('popup');
+const closeBtn = document.getElementById('popup-close-btn');
 
-const buttonElement = document.querySelector("project-img");
-
-buttonElement.addEventListener("click", () => {
-  window.location.href = "https://t.me/horus_vbot";
+closeBtn.addEventListener('click', function() {
+  // Close the popup with a smooth transition
+  popup.classList.remove('active');
 });
+  const emailForm = document.getElementById('emailForm');
+
+  // Get the existing CTA button
+  const ctaButton = document.getElementById('horus-pop-up');
+
+  ctaButton.addEventListener('click', function(e) {
+    // Prevent the default link behavior
+    e.preventDefault();
+  
+    // Show the popup with a smooth transition
+    popup.classList.add('active');
+  });
+
+  // Attach a click event listener to the CTA button
+  ctaButton.addEventListener('click', function(e) {
+    // Prevent the default link behavior
+    e.preventDefault();
+
+    // Show the popup
+    popup.style.display = 'block';
+  });
+
+  // Attach a submit event listener to the form
+  emailForm.addEventListener('submit', function(e) {
+    // Prevent the form from submitting in the traditional way
+    e.preventDefault();
+
+    // Get the entered email value
+    const userEmail = document.getElementById('email').value;
+
+    // Simple email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (emailRegex.test(userEmail)) {
+      // If the email is valid, redirect the user to the specified link
+      window.location.href = "https://t.me/horus_vbot";
+    } else {
+      // If the email is not valid, you can display an error message or take other actions
+      alert('Please enter a valid email address.');
+    }
+  });
